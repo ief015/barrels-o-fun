220eb42711e68c263343c48926c328be2c4b6f28
Nathan Cousins <ief015@gmail.com>
2021-12-21 01:20:28 -0500

    initial

---
+++ control.lua
@@ -0,0 +1,16 @@
+--control.lua
+
+script.on_event(defines.events.on_built_entity,
+	function(event)
+		local player = game.get_player(event.player_index) 
+		local ent = event.created_entity
+		if (
+			ent.name == 'pipe' or
+			ent.name == 'pipe-to-ground' or
+			ent.name == 'pump'
+		) then
+			player.surface.create_entity{name="explosion", position=ent.position, force="neutral"} 
+			ent.destroy()
+		end
+	end
+)
