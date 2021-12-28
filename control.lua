597154c9c14df023255e9b5cd0e43dc0c82c7438
Nathan Cousins <ief015@gmail.com>
2021-12-28 15:40:12 -0500

    robots can no longer construct filtered ents

--- control.lua
+++ control.lua
@@ -1,17 +1,25 @@
 --control.lua
 
-script.on_event(defines.events.on_built_entity,
-	function(event)
-		local player = game.get_player(event.player_index) 
-		local ent = event.created_entity
-		if (
-			ent.name == 'pipe' or
-			ent.name == 'pipe-to-ground' or
-			ent.name == 'pump' or
-			ent.name == 'storage-tank'
-		) then
-			player.surface.create_entity{name="explosion", position=ent.position, force="neutral"} 
-			ent.destroy()
-		end
+local function fails_filter(ent)
+	return (
+		ent.name == 'pipe' or
+		ent.name == 'pipe-to-ground' or
+		ent.name == 'pump' or
+		ent.name == 'storage-tank'
+	)
+end
+
+local function check_and_destroy(ent)
+	if fails_filter(ent) then
+		ent.surface.create_entity{name="explosion", position=ent.position, force="neutral"} 
+		ent.destroy()
 	end
-)
+end
+
+script.on_event(defines.events.on_built_entity, function(event)
+	check_and_destroy(event.created_entity)
+end)
+
+script.on_event(defines.events.on_robot_built_entity, function(event)
+	check_and_destroy(event.created_entity)
+end)
